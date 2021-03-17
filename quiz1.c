4f465239a088e883d613f7347a4503d4d22e1d77
KairC <kai0902@hotmail.com>
2021-03-17 17:02:25 +0800

    Implement Fisher-Yates shuffle

--- quiz1.c
+++ quiz1.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 typedef struct __node {
     int value;
     struct __node *next;
@@ -80,6 +81,51 @@ void print_list(node_t *head)
     printf("\n");
 }
 
+node_t *pick_entry(node_t **head, node_t *entry) {
+  node_t **indirect = head;
+
+  while ((*indirect) != entry)
+    indirect = &(*indirect)->next;
+
+  *indirect = entry->next;
+  entry->next = NULL;
+  return entry;
+}
+
+void add_entry_(node_t **head, node_t *entry) {
+  node_t **indirect = head;
+
+  while (*indirect)
+    indirect = &(*indirect)->next;
+
+  *indirect = entry;
+}
+
+void FisherYates_shuffle(node_t **head) {
+  /* Get length of linked list */
+  int len = 0;
+  for (node_t **indirect = head; (*indirect); indirect = &(*indirect)->next)
+    len++;
+  if (len == 1)
+    return;
+
+  node_t *new_head = NULL;
+  srand(time(NULL));
+  for (; len > 0; len--) {
+    /* Roll */
+    int roll = (rand() % len) + 1;
+
+    /* Strike-out */
+    node_t **strike_out = head;
+    for (; roll > 1; roll--, strike_out = &(*strike_out)->next)
+      /* iterate */;
+
+    add_entry_(&new_head, pick_entry(head, *strike_out));
+  }
+
+  *head = new_head;
+}
+
 int main(int argc, char const *argv[])
 {
     node_t *head = NULL;
@@ -92,6 +138,9 @@ int main(int argc, char const *argv[])
     add_entry(&head, 109);
     add_entry(&head, 110);
     add_entry(&head, 111);
+    add_entry(&head, 23);
+    add_entry(&head, 156);
+    add_entry(&head, 734);
 
     print_list(head);
 
@@ -112,5 +161,10 @@ int main(int argc, char const *argv[])
     reverse(&head);
     print_list(head);
 
+    printf("Before shuffle : ");
+    print_list(head);
+    FisherYates_shuffle(&head);
+    printf("after shuffle : ");
+    print_list(head);
     return 0;
 }
